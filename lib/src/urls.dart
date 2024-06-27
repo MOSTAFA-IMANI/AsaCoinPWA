0679b949eac0fbc8bf2dc49e88e7f15ee4f67b47
sajjad-dehnavi <dehnavi671@gmail.com>
2024-06-27 08:34:06 +0330

    fix some problem

--- lib/src/urls.dart
+++ lib/src/urls.dart
@@ -1,8 +1,8 @@
 class AppUrls {
   static const String aboutUs = "https://asacoine.com/about";
   static const String connectUs = "https://asacoine.com/contact";
-  static const String signUp = "https://panel.asacoine.com/signup/";
-  static const String login = "https://panel.asacoine.com/signin/";
+  static const String signUp = "https://panel.asacoine.com/auth/signup";
+  static const String login = "https://panel.asacoine.com/auth/signin";
   static const String baseUrl = "https://asacoine.com/";
   static const String baseUrlWithoutSchema = "asacoine.com";
 }
